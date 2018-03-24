2bde06d6595ff5f50ac6645470843fa6485352c4
Thiago Galvani <thiagopaiva99@gmail.com>
2018-03-24 18:35:40 -0300

    Added webpack bundle analyzer on the project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
+
+
 module.exports = {
+    webpack: function (config) {
+        if (ANALYZE) {
+            config.plugins.push(new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                analyzerPort: 8888,
+                openAnalyzer: true
+            }))
+        }
+
+        return config
+    },
     exportPathMap: function () {
         return {
             '/': { page: '/', query: { title: 'Home' } },
